21fa30326a7f746130618da30da91c8f5163855e
Rajesh Mondal <rajesh01749@gmail.com>
2025-08-02 00:41:07 +0600

    practice fetching single and multiple records using various techniques

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class QueryController extends Controller
-{
-    //
+class QueryController extends Controller {
+    // Retrieving All Rows
+    function allRows() {
+        $products = DB::table( 'products' )->get();
+        return $products;
+    }
+
+    // Retrieving Single Row
+    function singleRow() {
+        $result = DB::table( 'products' )->first();
+        return $result;
+    }
+
+    // Retrieving Single Row using Find Method
+    function findMethod() {
+        $result = DB::table( 'brands' )->find( 2 );
+        return $result;
+    }
+
+    // Retrieving List of Column Values
+    function pluckMethod() {
+        $result = DB::table( 'brands' )->pluck( 'brandName', "id" );
+        return $result;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\QueryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/allRows', [QueryController::class, 'allRows'] );
+Route::get( '/singleRow', [QueryController::class, 'singleRow'] );
+Route::get( '/findMethod', [QueryController::class, 'findMethod'] );
+Route::get( '/pluckMethod', [QueryController::class, 'pluckMethod'] );
