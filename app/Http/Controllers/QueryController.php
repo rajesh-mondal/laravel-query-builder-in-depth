afb5021d08232861090f19a0f7e3222bfcafae26
Rajesh Mondal <rajesh01749@gmail.com>
2025-08-02 15:25:13 +0600

    practice different types of join, select, and union methods

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Query\JoinClause;
 
 class QueryController extends Controller {
     // Retrieving All Rows
@@ -39,4 +40,57 @@ class QueryController extends Controller {
 
         return ['count' => $count, 'max' => $max, 'min' => $min, 'avg' => $avg, 'sum' => $sum];
     }
+
+    // Select Clause
+    function selectClauses() {
+        $result = DB::table( 'products' )->select( 'title', 'price', 'stock' )->get();
+        // $result = DB::table( 'products' )->select( 'title' )->distinct()->get();
+        return $result;
+    }
+
+    // Inner Join
+    function innerJoin() {
+        $products = DB::table( 'products' )
+            ->join( 'categories', 'products.category_id', '=', 'categories.id' )
+            ->join( 'brands', 'products.brand_id', '=', 'brands.id' )
+            ->get();
+        return $products;
+    }
+
+    // Left Join
+    function leftJoin() {
+        $products = DB::table( 'products' )
+            ->leftJoin( 'categories', 'products.category_id', '=', 'categories.id' )
+            ->leftJoin( 'brands', 'products.brand_id', '=', 'brands.id' )
+            ->get();
+        return $products;
+    }
+
+    // Cross Join
+    function crossJoin() {
+        $result = DB::table( 'products' )
+            ->crossJoin( 'brands' )
+            ->get();
+
+        return $result;
+    }
+
+    // Advanced Join Clauses
+    function advancedJoin() {
+        $result = DB::table( 'products' )
+            ->join( 'categories', function ( JoinClause $join ) {
+                $join->on( 'products.category_id', '=', 'categories.id' )
+                    ->where( 'products.price', '>', 2000 );
+            } )->get();
+
+        return $result;
+    }
+
+    // Unions
+    function union() {
+        $query = DB::table( 'products' )->where( 'products.price', '>', 2000 );
+        $otherQuery = DB::table( 'products' )->where( 'products.discount', '=', 1 )->union( $query )->get();
+        return $otherQuery;
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ Route::get( '/findMethod', [QueryController::class, 'findMethod'] );
 Route::get( '/pluckMethod', [QueryController::class, 'pluckMethod'] );
 
 Route::get( '/aggregates', [QueryController::class, 'aggregates'] );
+
+Route::get( '/selectClauses', [QueryController::class, 'selectClauses'] );
+
+Route::get( '/innerJoin', [QueryController::class, 'innerJoin'] );
+Route::get( '/leftJoin', [QueryController::class, 'leftJoin'] );
+Route::get( '/crossJoin', [QueryController::class, 'crossJoin'] );
+Route::get( '/advancedJoin', [QueryController::class, 'advancedJoin'] );
+
+Route::get( '/union', [QueryController::class, 'union'] );
